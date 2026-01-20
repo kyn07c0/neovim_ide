4dd11b62dc266d6a9f83255a33a5fafd5f711b79
Yuri <kyn07c0@yandex.ru>
2026-01-20 13:36:19 +0300

    Changed main settings

--- init.lua
+++ init.lua
@@ -1,3 +1,6 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 -- Бутстрап Lazy.nvim (если не установлен, клонируем его автоматически)
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -15,11 +18,11 @@ vim.opt.rtp:prepend(lazypath)
 -- Глобальные настройки Neovim
 vim.opt.number = true -- Включаем нумерацию строк
 vim.opt.relativenumber = true -- Относительная нумерация для удобства навигации
-vim.opt.tabstop = 4 -- Размер табуляции
-vim.opt.shiftwidth = 4 -- Сдвиг при отступах
+vim.opt.tabstop = 2 -- Размер табуляции
+vim.opt.shiftwidth = 2 -- Сдвиг при отступах
 vim.opt.expandtab = true -- Преобразование табов в пробелы
 vim.g.mapleader = " " -- Лидер-ключ для кастомных комбинаций (пробел)
-vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20" -- Блочный курсор для всех режимов
+vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:block,r-cr-o:hor20" -- Блочный курсор
 vim.opt.equalalways = false
 vim.opt.autochdir = false
 
@@ -29,9 +32,6 @@ vim.o.undofile = true -- включить persistent undo
 vim.o.undolevels = 10000 -- большой history
 vim.o.updatetime = 300 -- быстрее обновления CursorHold
 vim.o.timeoutlen = 500 -- таймаут для последовательных клавиш
-
--- Оптимизации LSP для C++
-vim.lsp.set_log_level("error") -- уменьшить логирование LSP
 
 -- Настраиваем Lazy.nvim для загрузки плагинов из директории lua/plugins
 require("lazy").setup("plugins")
