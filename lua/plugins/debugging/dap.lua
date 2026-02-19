c3f5f07a1d83519fbb3a7f28eb82be7ad14e596e
Yuri <kyn07c0@yandex.ru>
2026-02-19 16:39:09 +0300

    Debugger windows have been moved to the bottom

--- lua/plugins/debugging/dap.lua
+++ lua/plugins/debugging/dap.lua
@@ -53,26 +53,16 @@ return {
 				layouts = {
 					{
 						elements = {
-							{ id = "scopes", size = 0.25 },
-							{ id = "breakpoints", size = 0.25 },
-							{ id = "stacks", size = 0.25 },
-							{ id = "watches", size = 0.25 },
-						},
-						position = "left",
-						size = 40,
-					},
-					{
-						elements = {
 							{ id = "repl", size = 0.5 },
 							{ id = "console", size = 0.5 },
 						},
 						position = "bottom",
-						size = 10,
+						size = 15,
 					},
 				},
-				controls = {
-					element = "repl",
-					enabled = true,
+				render = {
+					max_type_length = nil,
+					max_value_lines = 100,
 				},
 			})
 
